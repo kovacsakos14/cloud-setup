a32a8f97ba00bc4515164b483f57c55f83eb2a6a
kovacsakos14 <kovacsakos12@gmail.com>
2022-07-18 20:30:20 +0000

    first

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "***** Configuring custom user, data and useful softwares *****"
+echo "***** Install stuff for ansible *****"
 
-echo "**** Create new user kovacsakos ****"
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install git curl python3.10 python3-apt python3.10-distutils python3.10-venv
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.10 get-pip.py
+python3 -m pip install --user ansible
 
+echo "**** Start setup with ansible ****"
 
+ansible-playbook playbook-20.04.yaml
