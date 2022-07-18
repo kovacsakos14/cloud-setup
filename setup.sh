4bfce2b51ea7504aa9176656a25a50c76d22cf80
kovacsakos14 <kovacsakos12@gmail.com>
2022-07-18 20:40:53 +0000

    first

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 echo "***** Install stuff for ansible *****"
 
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install git curl python3.10 python3-apt python3.10-distutils python3.10-venv
+sudo apt-get install -y git curl python3.10 python3-apt python3.10-distutils python3.10-venv
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.10 get-pip.py
+python3 get-pip.py
 python3 -m pip install --user ansible
 
 echo "**** Start setup with ansible ****"
